fe1c892f45302513e5b6f2a359834abb74b5d5ce
Simon Bowly <bowly@gurobi.com>
2021-11-29 15:23:58 +1100

    Silence all test environments
    Set gurobi.env to use silent parameter for all invocations in the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
 using Gurobi
 using Test
 
+const MOI = Gurobi.MOI
+
 # When adding new tests, be mindful about creating new environments. Either
 # re-use an existing environment in the module, or name the test function
 # `test_MULTI_ENV_xxx` to trap the specific Gurobi error indicating that an
 # environment could not be created.
-const GRB_ENV = Gurobi.Env()
+const GRB_ENV = Gurobi.Env(MOI.Silent(), true)
 
 @testset "MathOptInterface Tests" begin
     for file in readdir("MOI")
